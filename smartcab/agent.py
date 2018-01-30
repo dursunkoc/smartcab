620cdab4725dd7f2b0aa360380f0731d78f5307a
dursunkoc <dursunKoc@gmail.com>
2018-01-30 09:58:58 +0300

    q3 completed

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -101,7 +101,11 @@ class LearningAgent(Agent):
         # Set the agent state and default action
         self.state = state
         self.next_waypoint = self.planner.next_waypoint()
-        action = None
+        if self.learning:
+            # TODO should be modified as choose a random action with 'epsilon' probability
+            action = random.choice(self.valid_actions)
+        else:
+            action = random.choice(self.valid_actions)
 
         ########### 
         ## TO DO ##
@@ -151,7 +155,7 @@ def run():
     #   verbose     - set to True to display additional output from the simulation
     #   num_dummies - discrete number of dummy agents in the environment, default is 100
     #   grid_size   - discrete number of intersections (columns, rows), default is (8, 6)
-    env = Environment()
+    env = Environment(verbose = False, num_dummies=10, grid_size=(8,6))
     
     ##############
     # Create the driving agent
@@ -165,7 +169,7 @@ def run():
     # Follow the driving agent
     # Flags:
     #   enforce_deadline - set to True to enforce a deadline metric
-    env.set_primary_agent(agent)
+    env.set_primary_agent(agent, enforce_deadline=True)
 
     ##############
     # Create the simulation
@@ -174,14 +178,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env)
+    sim = Simulator(env, update_delay = 0.01, log_metrics=True)
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run()
+    sim.run(n_test=10)
 
 
 if __name__ == '__main__':
